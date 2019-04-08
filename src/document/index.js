jonnycrunch/js-ipid-1

feat: first implementation

src/document/index.js
@@ -1,0 +1,106 @@
+import { omitBy, isArray, isUndefined } from 'lodash';
+
+import service from './service';
+import publicKey from './publicKey';
+import authentication from './authentication';
+import { generateDocument, isEquivalentId } from './utils';
+
+class Document {
+    #content;
+
+    constructor({ content }) {
+        this.#content = {
+            publicKey: [],
+            authentication: [],
+            service: [],
+            ...content,
+        };
+    }
+
+    getContent() {
+        return omitBy(this.#content, (prop) => isUndefined(prop) || (isArray(prop) && prop.length === 0));
+    }
+
+    addPublicKey(props) {
+        props.id = publicKey.createId(this.#content.id, props.id);
+        props.controller = props.controller || this.#content.id;
+
+        publicKey.assert(props, this.#content.publicKey);
+
+        this.#content.publicKey.push(props);
+        this.#refreshUpdated();
+
+        return props;
+    }
+
+    revokePublicKey(id) {
+        const filter = this.#content.publicKey.filter((key) => !isEquivalentId(key.id, id, publicKey.separator));
+
+        if (this.#content.publicKey.length === filter.length) {
+            return;
+        }
+
+        this.revokeAuthentication(id);
+
+        this.#content.publicKey = filter;
+        this.#refreshUpdated();
+    }
+
+    addAuthentication(auth) {
+        const key = this.#content.publicKey.find((pk) => isEquivalentId(pk.id, auth, publicKey.separator)) || {};
+
+        authentication.assert(key.id, this.#content.authentication);
+
+        this.#content.authentication.push(key.id);
+        this.#refreshUpdated();
+
+        return key.id;
+    }
+
+    revokeAuthentication(id) {
+        const filter = this.#content.authentication.filter((auth) => !isEquivalentId(id, authentication.parseId(auth), publicKey.separator));
+
+        if (this.#content.authentication.length === filter.length) {
+            return;
+        }
+
+        this.#content.authentication = filter;
+        this.#refreshUpdated();
+    }
+
+    addService(props) {
+        props.id = service.createId(this.#content.id, props.id);
+
+        service.assert(props, this.#content.service);
+
+        this.#content.service.push(props);
+        this.#refreshUpdated();
+
+        return props;
+    }
+
+    revokeService(id) {
+        const filter = this.#content.service.filter((srvc) => !isEquivalentId(srvc.id, id, service.separator));
+
+        if (this.#content.service.length === filter.length) {
+            return;
+        }
+
+        this.#content.service = filter;
+        this.#refreshUpdated();
+    }
+
+    #refreshUpdated = () => {
+        this.#content.updated = new Date().toISOString();
+    }
+}
+
+const createDocument = async (key, content) => {
+    if (!content) {
+        content = await generateDocument(key);
+    }
+
+    return new Document({ content });
+};
+
+export default createDocument;

src/utils/errors.js
@@ -1,0 +1,89 @@
+class ErrorWithCode extends Error {
+    constructor(message, code) {
+        super(message);
+
+        Error.captureStackTrace(this, this.constructor);
+
+        Object.assign(this, { code });
+    }
+}
+
+// Authentication Based -------------------------------------
+
+export class DuplicateAuthentication extends ErrorWithCode {
+    constructor(id) {
+        super(`Authentication with same ${id} already exists.`, 'DUPLICATE_AUTHENTICATION');
+    }
+}
+
+export class InvalidAuthentication extends ErrorWithCode {
+    constructor(message) {
+        message = message || 'Invalid authentication.';
+
+        super(message, 'INVALID_AUTHENTICATION');
+    }
+}
+
+// ----------------------------------------------------------
+
+// Public Key Based -----------------------------------------
+
+export class DuplicatePublicKey extends ErrorWithCode {
+    constructor(id) {
+        super(`PublicKey with same ${id} already exists.`, 'DUPLICATE_PUBLICKEY');
+    }
+}
+
+export class InvalidPublicKey extends ErrorWithCode {
+    constructor(message) {
+        message = message || 'Invalid public key.';
+
+        super(message, 'INVALID_PUBLICKEY');
+    }
+}
+
+// ----------------------------------------------------------
+
+// Service Endpoint Based -----------------------------------
+
+export class DuplicateService extends ErrorWithCode {
+    constructor(id) {
+        super(`Service with same ${id} already exists.`, 'DUPLICATE_SERVICE');
+    }
+}
+
+export class InvalidService extends ErrorWithCode {
+    constructor(message) {
+        message = message || 'Invalid service.';
+
+        super(message, 'INVALID_SERVICE');
+    }
+}
+
+// ----------------------------------------------------------
+
+// IPFS/IPNS Based ------------------------------------------
+
+export class InvalidDID extends ErrorWithCode {
+    constructor(did) {
+        super(`Invalid DID: ${did}`, 'INVALID_DID');
+    }
+}
+
+export class IllegalCreate extends ErrorWithCode {
+    constructor(message) {
+        message = message || 'Document already exists.';
+
+        super(message, 'ILLEGAL_CREATE');
+    }
+}
+
+export class UnavailableIPFS extends ErrorWithCode {
+    constructor(message) {
+        message = message || 'IPFS node is unavailable.';
+
+        super(message, 'IPFS_UNAVAILABLE');
+    }
+}
+
+// ----------------------------------------------------------

ipfs-shipyard/js-did-ipid

src/document/index.js
@@ -1,0 +1,106 @@
+import { omitBy, isArray, isUndefined } from 'lodash';
+
+import service from './service';
+import publicKey from './publicKey';
+import authentication from './authentication';
+import { generateDocument, isEquivalentId } from './utils';
+
+class Document {
+    #content;
+
+    constructor({ content }) {
+        this.#content = {
+            publicKey: [],
+            authentication: [],
+            service: [],
+            ...content,
+        };
+    }
+
+    getContent() {
+        return omitBy(this.#content, (prop) => isUndefined(prop) || (isArray(prop) && prop.length === 0));
+    }
+
+    addPublicKey(props) {
+        props.id = publicKey.createId(this.#content.id, props.id);
+        props.controller = props.controller || this.#content.id;
+
+        publicKey.assert(props, this.#content.publicKey);
+
+        this.#content.publicKey.push(props);
+        this.#refreshUpdated();
+
+        return props;
+    }
+
+    revokePublicKey(id) {
+        const filter = this.#content.publicKey.filter((key) => !isEquivalentId(key.id, id, publicKey.separator));
+
+        if (this.#content.publicKey.length === filter.length) {
+            return;
+        }
+
+        this.removeAuthentication(id);
+
+        this.#content.publicKey = filter;
+        this.#refreshUpdated();
+    }
+
+    addAuthentication(auth) {
+        const key = this.#content.publicKey.find((pk) => isEquivalentId(pk.id, auth, publicKey.separator)) || {};
+
+        authentication.assert(key.id, this.#content.authentication);
+
+        this.#content.authentication.push(key.id);
+        this.#refreshUpdated();
+
+        return key.id;
+    }
+
+    removeAuthentication(id) {
+        const filter = this.#content.authentication.filter((auth) => !isEquivalentId(id, authentication.parseId(auth), publicKey.separator));
+
+        if (this.#content.authentication.length === filter.length) {
+            return;
+        }
+
+        this.#content.authentication = filter;
+        this.#refreshUpdated();
+    }
+
+    addService(props) {
+        props.id = service.createId(this.#content.id, props.id);
+
+        service.assert(props, this.#content.service);
+
+        this.#content.service.push(props);
+        this.#refreshUpdated();
+
+        return props;
+    }
+
+    removeService(id) {
+        const filter = this.#content.service.filter((srvc) => !isEquivalentId(srvc.id, id, service.separator));
+
+        if (this.#content.service.length === filter.length) {
+            return;
+        }
+
+        this.#content.service = filter;
+        this.#refreshUpdated();
+    }
+
+    #refreshUpdated = () => {
+        this.#content.updated = new Date().toISOString();
+    }
+}
+
+const createDocument = async (key, content) => {
+    if (!content) {
+        content = await generateDocument(key);
+    }
+
+    return new Document({ content });
+};
+
+export default createDocument;

src/utils/errors.js
@@ -1,0 +1,89 @@
+class BaseError extends Error {
+    constructor(message, code) {
+        super(message);
+
+        Error.captureStackTrace(this, this.constructor);
+
+        Object.assign(this, { code });
+    }
+}
+
+// Authentication Based -------------------------------------
+
+export class DuplicateAuthentication extends BaseError {
+    constructor(id) {
+        super(`Authentication with same ${id} already exists.`, 'DUPLICATE_AUTHENTICATION');
+    }
+}
+
+export class InvalidAuthentication extends BaseError {
+    constructor(message) {
+        message = message || 'Invalid authentication.';
+
+        super(message, 'INVALID_AUTHENTICATION');
+    }
+}
+
+// ----------------------------------------------------------
+
+// Public Key Based -----------------------------------------
+
+export class DuplicatePublicKey extends BaseError {
+    constructor(id) {
+        super(`PublicKey with same ${id} already exists.`, 'DUPLICATE_PUBLICKEY');
+    }
+}
+
+export class InvalidPublicKey extends BaseError {
+    constructor(message) {
+        message = message || 'Invalid public key.';
+
+        super(message, 'INVALID_PUBLICKEY');
+    }
+}
+
+// ----------------------------------------------------------
+
+// Service Endpoint Based -----------------------------------
+
+export class DuplicateService extends BaseError {
+    constructor(id) {
+        super(`Service with same ${id} already exists.`, 'DUPLICATE_SERVICE');
+    }
+}
+
+export class InvalidService extends BaseError {
+    constructor(message) {
+        message = message || 'Invalid service.';
+
+        super(message, 'INVALID_SERVICE');
+    }
+}
+
+// ----------------------------------------------------------
+
+// IPFS/IPNS Based ------------------------------------------
+
+export class InvalidDid extends BaseError {
+    constructor(did) {
+        super(`Invalid DID: ${did}`, 'INVALID_DID');
+    }
+}
+
+export class IllegalCreate extends BaseError {
+    constructor(message) {
+        message = message || 'Document already exists.';
+
+        super(message, 'ILLEGAL_CREATE');
+    }
+}
+
+export class UnavailableIpfs extends BaseError {
+    constructor(message) {
+        message = message || 'IPFS node is unavailable.';
+
+        super(message, 'IPFS_UNAVAILABLE');
+    }
+}
+
+// ----------------------------------------------------------